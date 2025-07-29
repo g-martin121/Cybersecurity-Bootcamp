cdd1f01da202caa58dfe36f8d8eb0013a5661933
GiaVonna Martin <martingiavonna@yahoo.com>
2025-07-28 23:19:26 -0500

    even_odd.py

---
+++ even_odd.py
@@ -0,0 +1,17 @@
+number= int(input("Enter a number:"))
+if number % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+
+
+while True:
+    number= int(input("Enter a number:"))
+    if number % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+    if input("Quit? (y/n):"). lower() == "y":
+        break
