d8ba2239583a8a25d956030c1d38daee7fdd2b6d
GiaVonna Martin <martingiavonna@yahoo.com>
2025-08-13 21:31:32 -0500

    list directories

---
+++ coding_challenge.py
@@ -0,0 +1,22 @@
+#import operating system
+import os
+
+def list_directories(path):
+    try:
+        #items is everything listed in the path you choose
+        items = os.listdir(path)
+        print (f"Directories in {path}:")
+        for item in items:
+            #take every path and item if its a folder and print it
+            item_path = os.path.join(path, item)
+            if os.path.isdir(item_path):
+                print(item)
+    except FileNotFoundError:
+        print(f"Directory '{path}' not found")
+    except PermissionError:
+        print(f"Permission denied for '{path}'")
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        
+if __name__ == "__main__":
+    list_directories("C:\\")
