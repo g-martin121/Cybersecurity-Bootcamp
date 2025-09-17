b898e47b2c9a715bf76abae58f370f514ace9875
GiaVonna Martin <martingiavonna@yahoo.com>
2025-09-16 22:01:10 -0500

    .

---
+++ coding_challenges/monitor_connections.py
@@ -0,0 +1,24 @@
+import psutil
+from datetime import datetime
+import time
+
+def monitor_connections():
+    try:
+        for i in range(3):
+            timestamp = datetime.now().strftime("%Y%m%d %H:%M:%S")
+            print(f"Current time: {timestamp}")
+            connections = psutil.net_connections(kind="tcp")
+
+            for conn in connections:
+                print(f" IP: {conn.laddr.ip} Port: {conn.laddr.port}")
+            time.sleep(10)
+
+    except PermissionError:
+        print("Permission denied")
+    except psutil.AccessDenied:
+        print("Access denied.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    monitor_connections()
