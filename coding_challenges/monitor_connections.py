4a273660451cd92371f8c88c0a13491d1239c26d
GiaVonna Martin <martingiavonna@yahoo.com>
2025-09-18 20:17:18 -0500

    .

--- coding_challenges/monitor_connections.py
+++ coding_challenges/monitor_connections.py
@@ -5,18 +5,21 @@ import time
 def monitor_connections():
     try:
         for i in range(3):
-            timestamp = datetime.now().strftime("%Y%m%d %H:%M:%S")
-            print(f"\nCurrent time: {timestamp}")
-            connections = psutil.net_connections(kind="tcp")
+            timestamp = datetime.now().strftime("%m%d%Y %H:%M:%S")
+            print(f"\nScan {i+1} at {timestamp} (Layer 4: transport)")
 
-            for conn in connections:
-                print(f" IP: {conn.laddr.ip} Port: {conn.laddr.port}")
+            for conn in psutil.net_connections(kind="tcp"):
+                if conn.status == "ESTABLISHED":
+                    local = f"{conn.laddr.ip}: {conn.laddr.port}"
+                    remote = f"{conn.raddr.ip}: {conn.raddr.port}" if conn.raddr else "None"
+                    print(f"Local: {local}, Remote: {remote}")
+
             time.sleep(10)
-
-    except PermissionError:
-        print("Permission denied")
+            
     except psutil.AccessDenied:
         print("Access denied.")
+    except PermissionError:
+        print("Run as administrator.")
     except Exception as e:
         print(f"Error: {e}")
 
